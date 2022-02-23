7369c0a8602cc758a9da6e970e5b2dc844835831
isaacapo <55719570+isaacapo@users.noreply.github.com>
2022-02-22 18:25:58 -0700

    add short experiments
    adding simple work done exploring save and idle mechanics in js

---
+++ JS/save.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+const playerName = document.QuerySelector('#playerName');
+const cb = document.querySelector('#saveExists');
+console.log(cb.checked);
+
+<label for="accept">
+   <input type="checkbox" id="accept" name="accept" value="yes"> Accept 
+</label>
