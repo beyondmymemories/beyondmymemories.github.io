50f4bfb3d2ee7afe92287ccf88083b8f3b0085de
isaacapo <55719570+isaacapo@users.noreply.github.com>
2022-02-24 19:38:29 -0700

    Add idle mechanic test
    Idle mechanic saves time exited to localStorage on beginIdle() call and finds the difference of time when endIdle() is called in seconds. idle.js will have more functions to work with that time.

--- JS/idle.js
+++ JS/idle.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+/*
+	86400 seconds / day
+	3600 seconds / hour
+  	60 seconds / minute
+	1 seconds / second lmao
+
+*/
+ function beginIdle() {
+	 saveVar("exitTime", Date.now());
+ }
+
+function endIdle() {
+	var enterTime = Date.now();
+	
+	var timeIdle = enterTime - loadVar("exitTime");
+	document.getElementById("idleOutput").innerHTML = translateSeconds(Math.round(timeIdle/1000));
+}
+
+function translateSeconds(secondsIn) {	
+	return new Date(secondsIn * 1000).toISOString().substr(11, 8);	
+}
