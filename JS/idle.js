3e86435609fb90059d00451a48dae8a35bf36df8
isaacapo <55719570+isaacapo@users.noreply.github.com>
2022-03-02 14:28:46 -0700

    Add save and Idle test
    added save and idle test and idle functionality through the use of dates

--- JS/idle.js
+++ JS/idle.js
@@ -6,17 +6,26 @@
 	1 seconds / second lmao
 
 */
- function beginIdle() {
-	 saveVar("exitTime", Date.now());
- }
+function beginIdle() {
+	saveVar("exitTime", Date.now());
+}
 
 function endIdle() {
+	
+}
+
+
+function displayTimeIdle() {
 	var enterTime = Date.now();
 	
 	var timeIdle = enterTime - loadVar("exitTime");
 	document.getElementById("idleOutput").innerHTML = translateSeconds(Math.round(timeIdle/1000));
 }
-
 function translateSeconds(secondsIn) {	
 	return new Date(secondsIn * 1000).toISOString().substr(11, 8);	
+}
+
+function applyIdle() {
+	number(playerGold) = number(playerGold) + number(5);
+	//alert(playerGold.concat(" + ", endIdle(), " = ", (playerGold + endIdle())));
 }
