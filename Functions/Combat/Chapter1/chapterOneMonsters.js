1a1fe6f061773d0bec1c64012b4b32f1223e13e7
Miguel Fernandez <mfrnandz@nmsu.edu>
2022-03-03 20:21:48 -0700

    added libraries for chapter 1
    Added some weapons, monsters, and

---
+++ Functions/Combat/Chapter1/chapterOneMonsters.js
@@ -0,0 +1,26 @@
+var Monsters = {
+    Goblins: {
+        'Archer': {
+            type: 'ranged',
+            weapon: 'Shortbow',
+            health: 9
+        },
+        'Melee': {
+            type: 'melee',
+            weapon: 'Shortsword',
+            health: 12
+        },
+        'Hobgoblin': {
+            type: 'melee',
+            weapon: 'Club',
+            health: 30
+        }
+    },
+    Beasts: {
+        'Minotaur': {
+            type: 'melee',
+            weapon: 'Greataxe',
+            health: 50
+        }
+    }
+}
