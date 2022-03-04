886c46535cdb1b7a2131c3b1ee055ca5b4a05cd1
Miguel Fernandez <mfrnandz@nmsu.edu>
2022-03-03 20:34:46 -0700

    Updated weapons damage and type

--- Functions/Combat/Chapter1/chapterOneMonsters.js
+++ Functions/Combat/Chapter1/chapterOneMonsters.js
@@ -1,25 +1,25 @@
-var Monsters = {
+var Monsters1 = {
     Goblins: {
         'Archer': {
             type: 'ranged',
-            weapon: 'Shortbow',
+            weapon: Attacks1.Weapons.Shortbow,
             health: 9
         },
         'Melee': {
             type: 'melee',
-            weapon: 'Shortsword',
+            weapon: Attacks1.Weapons.Shortsword,
             health: 12
         },
         'Hobgoblin': {
             type: 'melee',
-            weapon: 'Club',
+            weapon: Attacks1.Weapons.Club,
             health: 30
         }
     },
     Beasts: {
         'Minotaur': {
             type: 'melee',
-            weapon: 'Greataxe',
+            weapon: Attacks1.Weapons.Greataxe,
             health: 50
         }
     }
