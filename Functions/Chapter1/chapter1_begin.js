64311770e13675f23672016cb0a0cbe0bc364c7a
Alayna Randall <alaynar@nmsu.edu>
2022-03-16 16:15:36 -0600

    Working on testing buttons
    and rewriting chatlog script

--- Functions/Chapter1/chapter1_begin.js
+++ Functions/Chapter1/chapter1_begin.js
@@ -5,22 +5,27 @@
 
 function selectOption() {
 
+    //Call the beginning of the game
+    updateChatLog('../Functions/Chapter1/chapter1callscript.txt','//START//');
 
-    document.getElementById("1").innerHTML="Test1"
+    document.getElementById("1").innerHTML="Waterfall Room"
     document.getElementById("BUTTONTESTING").innerHTML="Search Around You"
     document.getElementById("3").innerHTML="Test3"
-    document.getElementById("4").innerHTML="Test4"
+    document.getElementById("4").innerHTML="Combat"
 
-    //document.getElementById("2").('onclick', updateChatLog('Chapter 1 Script.txt','//START//'));
-    //document.getElementById("BUTTONTESTING").onclick = updateChatLog('../Functions/Chapter1/Chapter 1 Script.txt','//START//');
+    document.getElementById("1").addEventListener("click", function(event) {
+        //Change photo
+
+    });
+    
     document.getElementById("BUTTONTESTING").addEventListener("click", function(event) {
         updateChatLog('../Functions/Chapter1/Chapter 1 Script.txt','//START//');
     });
 
+    document.getElementById("4").addEventListener("click", function(event) {
+        document.getElementById("container3").style.display = "none"
+        document.getElementById("container4").style.display = "inline-block"
+    });
 
-    //Testing to call start information
-//<button class="chatbtndemo" onclick="updateChatLog('testingScript.txt','//1.1.1//')">Sit and Wait</button>
-
-    //updateChatLog('../Functions/Chapter1/Chapter 1 Script.txt','//START//');
     
-}
+} //end of select option
