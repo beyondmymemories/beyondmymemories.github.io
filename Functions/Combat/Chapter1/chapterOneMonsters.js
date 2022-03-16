34c59b5ec04dc6bd48e7379418f41dcca82a52e7
Miguel Fernandez <54916306+MigglesHere@users.noreply.github.com>
2022-03-16 16:13:12 -0600

    Added stats to the enemies

--- Functions/Combat/Chapter1/chapterOneMonsters.js
+++ Functions/Combat/Chapter1/chapterOneMonsters.js
@@ -3,24 +3,56 @@ var Monsters1 = {
         'Archer': {
             type: 'ranged',
             weapon: Attacks1.Weapons.Shortbow,
-            health: 9
+            health: 9,
+            stats: {
+                str: 1,
+                dex: 3,
+                con: 1,
+                int: -2,
+                wis: -1,
+                chr: 1
+            }
         },
         'Melee': {
             type: 'melee',
             weapon: Attacks1.Weapons.Shortsword,
-            health: 12
+            health: 12,
+            stats:{
+                str: 3,
+                dex: 1,
+                con: 1,
+                int: -2,
+                wis: -1,
+                chr: 1
+            }
         },
         'Hobgoblin': {
             type: 'melee',
-            weapon: Attacks1.Weapons.Club,
-            health: 30
+            weapon: Attacks1.Weapons.Maul,
+            health: 30,
+            stats: {
+                str: 4,
+                dex: 1,
+                con: 3,
+                int: -2,
+                wis: -1,
+                chr: 0
+            }
         }
     },
     Beasts: {
         'Minotaur': {
             type: 'melee',
             weapon: Attacks1.Weapons.Greataxe,
-            health: 50
+            health: 50,
+            stats: {
+                str: 5,
+                dex: 2,
+                con: 4,
+                int: -2,
+                wis: -2,
+                chr: 0
+            }
         }
     }
 }
