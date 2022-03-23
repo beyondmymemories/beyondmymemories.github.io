d39f89cc93bc50537aa011a6025bbfb9ba4042a3
Miguel Fernandez <54916306+MigglesHere@users.noreply.github.com>
2022-03-23 16:12:15 -0600

    Update chapter1Stats.js

--- Functions/Chapter1/chapter1Stats.js
+++ Functions/Chapter1/chapter1Stats.js
@@ -1,38 +1,50 @@
 var charStats1 = {
    'Barbarian': {
-     type: 'Character',
-     str: 4,
-     dex: 0,
-     con: 3,
-     int: -1,
-     wis: 1,
-     chr: 2
+      type: 'Character',
+      stats: {
+         str: 4,
+         dex: 0,
+         con: 3,
+         int: -1,
+         wis: 1,
+         chr: 2
+      },
+      maxhp: 50
    },
    'Wizard': {
-     type: 'Character',
-     str: -1,
-     dex: 2,
-     con: 0,
-     int: 4,
-     wis: 3,
-     chr: 1
+      type: 'Character',
+      stats: {
+         str: -1,
+         dex: 2,
+         con: 0,
+         int: 4,
+         wis: 3,
+         chr: 1
+      },
+      maxhp: 30
+      
    },
    'Bard': {
-     type: 'Character',
-     str: 0,
-     dex: 3,
-     con: 2,
-     int: -1,
-     wis: 1,
-     chr: 4
+      type: 'Character',
+      stats: {
+         str: 0,
+         dex: 3,
+         con: 2,
+         int: -1,
+         wis: 1,
+         chr: 4
+      },
+      maxhp: 30
    },
    'Rogue': {
-     type: 'Character',
-     str: 1,
-     dex: 4,
-     con: 0,
-     int: -1,
-     wis: 3,
-     chr: 2
+      type: 'Character',
+      stats: {
+         str: 1,
+         dex: 4,
+         con: 0,
+         int: -1,
+         wis: 3,
+         chr: 2
+      }
    }
 }
