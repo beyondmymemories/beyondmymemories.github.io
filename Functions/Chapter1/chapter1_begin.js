5d9e7a87eacf331db21cb960536087b4cab6e39d
Miguel Fernandez <mfrnandz@nmsu.edu>
2022-05-05 22:30:54 -0600

    Update chapter1_begin.js

--- Functions/Chapter1/chapter1_begin.js
+++ Functions/Chapter1/chapter1_begin.js
@@ -92,9 +92,7 @@ function comResult(monster, result) {
     //document.getElementById("container4").style.display = "none";
     //document.getElementById("container3").style.display = "inline-block";
     updateChatLog('../Functions/Chapter1/chapter1callscript.txt', '//dashes//')
-    console.log("Before: " + getLiveVar("playerGold"))
-    addGold(500);
-    console.log("After: " + getLiveVar("playerGold"))
+    addGold(monster.gold);
     if (result == "Success0"){
         //check this: !currentVars.lootAdventurer
         printChatNode(comSucc);
